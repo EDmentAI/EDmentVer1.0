ec3f13e87e82bae3bbcc9f7cc5589cde95e13574
EDmentAI <rsedment@gmail.com>
2024-06-27 01:30:18 +0200

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -1,9 +1,9 @@
-document.getElementById('homework-icon').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent the default anchor behavior
-    const hiddenDiv = document.getElementById('homework');
-    if (hiddenDiv.style.display === 'none' || hiddenDiv.style.display === '') {
-        hiddenDiv.style.display = 'block';
-    } else {
-        hiddenDiv.style.display = 'none';
-    }
-});
+// document.getElementById('homework-icon').addEventListener('click', function(event) {
+//     event.preventDefault(); // Prevent the default anchor behavior
+//     const hiddenDiv = document.getElementById('homework');
+//     if (hiddenDiv.style.display === 'none' || hiddenDiv.style.display === '') {
+//         hiddenDiv.style.display = 'block';
+//     } else {
+//         hiddenDiv.style.display = 'none';
+//     }
+// });
